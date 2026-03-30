2dcf801a86a3f05a946120ef7b8dc84698fb18d1
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-03-30 14:31:34 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MuseumDialogueApp from './MuseumDialogueApp';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-slate-50">
+      <main className="flex-1">
+        <MuseumDialogueApp />
+      </main>
+    </div>
+  );
+}
